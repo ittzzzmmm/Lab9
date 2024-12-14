72738cf0060c5144e8a2d946445302d3d116ee14
ittzzzmmm <ittzzzmmm@gmail.com>
2024-12-14 16:44:22 +0700

    ittzm

--- lab9_1.cpp
+++ lab9_1.cpp
@@ -3,10 +3,13 @@
 using namespace std;
 
 int main(){	
+	double loan , rate , pay ;
 	cout << "Enter initial loan: ";
+	cin >> loan ;
 	cout << "Enter interest rate per year (%): ";
+	cin >> rate ;
 	cout << "Enter amount you can pay per year: ";
-
+	cin >> pay ;
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
 	//Try to change from 'left' to 'right' and see the effect
@@ -18,16 +21,61 @@ int main(){
 	cout << setw(13) << left << "NewBalance";
 	cout << "\n";
 	
-	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
-	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
 	
-	return 0;
+	double interest , PrevBalance , total , newBalance ;
+	PrevBalance = loan ;
+	interest = PrevBalance*(rate/100.00) ;
+	total = PrevBalance + interest ;
+	newBalance = total - pay ;
+	
+
+	if(total <= pay){
+		
+		pay = total ;
+		
+		cout << fixed << setprecision(2);
+		cout << setw(13) << left << 1 ;
+		cout << setw(13) << left << PrevBalance ;
+		cout << setw(13) << left << interest ;
+		cout << setw(13) << left << total ;
+		cout << setw(13) << left << pay ;
+		newBalance = pay - total ;
+		cout << setw(13) << left << newBalance << endl ;
+
+	}else{
+		
+		int i = 1 ;
+
+		do{
+			cout << fixed << setprecision(2);
+			cout << setw(13) << left << i ;
+			cout << setw(13) << left << PrevBalance ;
+			cout << setw(13) << left << interest ;
+			cout << setw(13) << left << total ;
+			cout << setw(13) << left << pay ;
+			cout << setw(13) << left << newBalance << endl ;
+			i++ ;
+
+			PrevBalance = newBalance ;
+			interest = PrevBalance*(rate/100) ;
+			total = PrevBalance + interest ;
+			newBalance = total - pay ;
+			if(newBalance < 0){
+				break ;
+			}
+		}while(true) ;
+
+		pay = total ;
+		newBalance = pay - total ;
+
+		cout << setw(13) << left << i ;
+		cout << setw(13) << left << PrevBalance ;
+		cout << setw(13) << left << interest ;
+		cout << setw(13) << left << pay ;
+		cout << setw(13) << left << pay ;
+		cout << setw(13) << left << newBalance ;
+
+	}
+
+
 }
--- lab9_2.cpp
+++ lab9_2.cpp
@@ -3,8 +3,23 @@ using namespace std;
 
 //Write the function printO() here
 
+	void printO(int N, int M)
+	{
+    	if(N>0 && M>0){
+        	for(int i = 1; i <=N; i++){
+            	for(int j =1; j <=M; j++){
+                	cout << "O" ;
+					}
+				cout << "\n" ;
+				}
+	}else{
+		cout << "Invalid input" ;
+	}
+	return;
+	}
+
 int main(){
-	
+
 	printO(2,2);
 	cout << "\n";
 	
@@ -21,4 +36,5 @@ int main(){
 	cout << "\n";	
 	
 	return 0;
+	
 }
